b40ce3dfe1275b60065c1feb002f26c2ab8071c2
TannerBluhm <tbluhm@live.unc.edu>
2019-10-29 16:15:28 -0400

    adding the 8x8 grid

---
+++ 8x8 Grid/render.js
@@ -0,0 +1,17 @@
+let game;
+let size = 0;
+let gs;
+let winningGame = {
+    board: [1024,1024,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    score: 4506,
+    won: false,
+    over: false,
+};
+
+$(document).ready( function () {
+    $('.tile').val("0");
+    $('.tile').append("<p>Hey</p>");
+    $('.tile').click(function(event) {
+        alert(this.id);
+    });
+});
